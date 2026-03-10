ff1fb56146f9307178e78df19bf5fd89bb1d7ed9
sam-pearmain <sam.pearmain@kcl.ac.uk>
2026-03-10 19:57:44 +0000

    erm

--- cases/di-vanna-inlet-mach-5/mesh-structured.py
+++ cases/di-vanna-inlet-mach-5/mesh-structured.py
@@ -1,4 +1,4 @@
-from math import cos, radians, sin, tan
+from math import radians, tan
 
 import gmsh
 
@@ -30,29 +30,19 @@ def main():
         tan(radians(ramp_angle_one)) - tan(radians(ramp_angle_two))
     )
     kink_height = tan(radians(ramp_angle_one)) * kink_length
+
     y_split = ramp_height + throat_height
+    blunt_height = 1.0 * scale_factor
+    y_split_top = y_split + blunt_height
 
     x_start = 0.0
     x_ramp_start = domain_length - intake_length
     x_kink = x_ramp_start + kink_length
     x_throat_start = x_ramp_start + ramp_length
 
-    cowl_sharp_x = x_throat_start + (
-        (intake_height - y_split) / tan(radians(cowl_angle))
+    x_cowl_tip = x_throat_start + (
+        (intake_height - y_split_top) / tan(radians(cowl_angle))
     )
-
-    r_blunt = 0.5 * scale_factor
-    theta = radians(cowl_angle)
-
-    xc = cowl_sharp_x + r_blunt / sin(theta / 2.0)
-    yc = intake_height - r_blunt
-
-    x_cowl_bot = xc - r_blunt * sin(theta)
-    y_cowl_bot = yc - r_blunt * cos(theta)
-
-    x_cowl_top = xc
-    y_cowl_top = yc + r_blunt
-
     x_end = domain_length
     z_extrude = 0.1
 
@@ -62,12 +52,12 @@ def main():
     nx_1 = int(round((x_ramp_start - x_start) / dx_constant))
     nx_2 = int(round((x_kink - x_ramp_start) / dx_constant))
     nx_3 = int(round((x_throat_start - x_kink) / dx_constant))
-    nx_cowl_front = int(round((x_cowl_bot - x_throat_start) / dx_constant))
-    nx_cowl_arc = 10
-    nx_wake = int(round((x_end - x_cowl_top) / dx_constant))
-    nx_throat = nx_cowl_front + nx_cowl_arc + nx_wake - 2
+    nx_cowl = int(round((x_cowl_tip - x_throat_start) / dx_constant))
+    nx_wake = int(round((x_end - x_cowl_tip) / dx_constant))
+    nx_throat = nx_cowl + nx_wake - 1
 
     ny_bottom = 380
+    ny_mid = 10
     ny_top = 120
     nz_total = 80
 
@@ -80,134 +70,126 @@ def main():
     p6 = geom.addPoint(x_start, y_split, 0)
     p7 = geom.addPoint(x_ramp_start, y_split, 0)
     p8 = geom.addPoint(x_kink, y_split, 0)
-    p9 = geom.addPoint(x_throat_start, y_split, 0)
+    p9_bot = geom.addPoint(x_throat_start, y_split, 0)
 
-    p10_bot = geom.addPoint(x_cowl_bot, y_cowl_bot, 0)
-    p10_center = geom.addPoint(xc, yc, 0)
-    p10_top = geom.addPoint(x_cowl_top, y_cowl_top, 0)
+    p_mt1 = geom.addPoint(x_start, y_split_top, 0)
+    p_mt2 = geom.addPoint(x_ramp_start, y_split_top, 0)
+    p_mt3 = geom.addPoint(x_kink, y_split_top, 0)
+    p9_top = geom.addPoint(x_throat_start, y_split_top, 0)
 
-    p11 = geom.addPoint(x_end, y_cowl_top, 0)
+    p10 = geom.addPoint(x_cowl_tip, intake_height, 0)
+    p11 = geom.addPoint(x_end, intake_height, 0)
     p12 = geom.addPoint(x_end, y_split, 0)
 
     p13 = geom.addPoint(x_start, domain_height, 0)
     p14 = geom.addPoint(x_ramp_start, domain_height, 0)
     p15 = geom.addPoint(x_kink, domain_height, 0)
     p16 = geom.addPoint(x_throat_start, domain_height, 0)
-    p17_bot = geom.addPoint(x_cowl_bot, domain_height, 0)
-    p17_top = geom.addPoint(x_cowl_top, domain_height, 0)
+    p17 = geom.addPoint(x_cowl_tip, domain_height, 0)
     p18 = geom.addPoint(x_end, domain_height, 0)
 
-    l_bottom = geom.addLine(p1, p2)
-    l_wall_1 = geom.addLine(p2, p3)
-    l_wall_2 = geom.addLine(p3, p4)
-    l_throat_bot = geom.addLine(p4, p5)
+    l_b1 = geom.addLine(p1, p2)
+    l_b2 = geom.addLine(p2, p3)
+    l_b3 = geom.addLine(p3, p4)
+    l_b4 = geom.addLine(p4, p5)
 
-    l_mid_1 = geom.addLine(p6, p7)
-    l_mid_2 = geom.addLine(p7, p8)
-    l_mid_3 = geom.addLine(p8, p9)
-    l_throat_top = geom.addLine(p12, p9)
+    l_sb1 = geom.addLine(p6, p7)
+    l_sb2 = geom.addLine(p7, p8)
+    l_sb3 = geom.addLine(p8, p9_bot)
+    l_sb4 = geom.addLine(p9_bot, p12)
 
-    l_cowl_front = geom.addLine(p9, p10_bot)
-    l_cowl_arc = geom.addCircleArc(p10_bot, p10_center, p10_top)
-    l_cowl_back = geom.addLine(p10_top, p11)
+    l_st1 = geom.addLine(p_mt1, p_mt2)
+    l_st2 = geom.addLine(p_mt2, p_mt3)
+    l_st3 = geom.addLine(p_mt3, p9_top)
 
-    l_top_1 = geom.addLine(p13, p14)
-    l_top_2 = geom.addLine(p14, p15)
-    l_top_3 = geom.addLine(p15, p16)
-    l_top_4_front = geom.addLine(p16, p17_bot)
-    l_top_4_arc = geom.addLine(p17_bot, p17_top)
-    l_top_5 = geom.addLine(p17_top, p18)
+    l_ct1 = geom.addLine(p9_top, p10)
+    l_ct2 = geom.addLine(p10, p11)
 
-    l_inlet_bot = geom.addLine(p1, p6)
-    l_inlet_top = geom.addLine(p6, p13)
-    l_v1_bot = geom.addLine(p2, p7)
-    l_v1_top = geom.addLine(p7, p14)
-    l_v2_bot = geom.addLine(p3, p8)
-    l_v2_top = geom.addLine(p8, p15)
-    l_throat_inlet = geom.addLine(p4, p9)
-    l_v3 = geom.addLine(p9, p16)
+    l_t1 = geom.addLine(p13, p14)
+    l_t2 = geom.addLine(p14, p15)
+    l_t3 = geom.addLine(p15, p16)
+    l_t4 = geom.addLine(p16, p17)
+    l_t5 = geom.addLine(p17, p18)
 
-    l_v4_bot = geom.addLine(p10_bot, p17_bot)
-    l_v4_top = geom.addLine(p10_top, p17_top)
+    l_v0_b = geom.addLine(p1, p6)
+    l_v1_b = geom.addLine(p2, p7)
+    l_v2_b = geom.addLine(p3, p8)
+    l_v3_b = geom.addLine(p4, p9_bot)
+    l_v5_b = geom.addLine(p5, p12)
 
-    l_out_int = geom.addLine(p5, p12)
-    l_out_ext = geom.addLine(p11, p18)
+    l_v0_m = geom.addLine(p6, p_mt1)
+    l_v1_m = geom.addLine(p7, p_mt2)
+    l_v2_m = geom.addLine(p8, p_mt3)
+    l_v3_m = geom.addLine(p9_bot, p9_top)
 
-    cl1_b = geom.addCurveLoop([l_bottom, l_v1_bot, -l_mid_1, -l_inlet_bot])
-    s1_b = geom.addPlaneSurface([cl1_b])
-    cl2_b = geom.addCurveLoop([l_wall_1, l_v2_bot, -l_mid_2, -l_v1_bot])
-    s2_b = geom.addPlaneSurface([cl2_b])
-    cl3_b = geom.addCurveLoop([l_wall_2, l_throat_inlet, -l_mid_3, -l_v2_bot])
-    s3_b = geom.addPlaneSurface([cl3_b])
-    cl4 = geom.addCurveLoop([l_throat_bot, l_out_int, l_throat_top, -l_throat_inlet])
-    s4 = geom.addPlaneSurface([cl4])
+    l_v0_t = geom.addLine(p_mt1, p13)
+    l_v1_t = geom.addLine(p_mt2, p14)
+    l_v2_t = geom.addLine(p_mt3, p15)
+    l_v3_t = geom.addLine(p9_top, p16)
+    l_v4 = geom.addLine(p10, p17)
+    l_v5_t = geom.addLine(p11, p18)
 
-    cl1_t = geom.addCurveLoop([l_mid_1, l_v1_top, -l_top_1, -l_inlet_top])
-    s1_t = geom.addPlaneSurface([cl1_t])
-    cl2_t = geom.addCurveLoop([l_mid_2, l_v2_top, -l_top_2, -l_v1_top])
-    s2_t = geom.addPlaneSurface([cl2_t])
-    cl3_t = geom.addCurveLoop([l_mid_3, l_v3, -l_top_3, -l_v2_top])
-    s3_t = geom.addPlaneSurface([cl3_t])
+    s1 = geom.addPlaneSurface([geom.addCurveLoop([l_b1, l_v1_b, -l_sb1, -l_v0_b])])
+    s2 = geom.addPlaneSurface([geom.addCurveLoop([l_b2, l_v2_b, -l_sb2, -l_v1_b])])
+    s3 = geom.addPlaneSurface([geom.addCurveLoop([l_b3, l_v3_b, -l_sb3, -l_v2_b])])
+    s4 = geom.addPlaneSurface([geom.addCurveLoop([l_b4, l_v5_b, -l_sb4, -l_v3_b])])
 
-    cl5_front = geom.addCurveLoop([l_cowl_front, l_v4_bot, -l_top_4_front, -l_v3])
-    s5_front = geom.addPlaneSurface([cl5_front])
+    s5 = geom.addPlaneSurface([geom.addCurveLoop([l_sb1, l_v1_m, -l_st1, -l_v0_m])])
+    s6 = geom.addPlaneSurface([geom.addCurveLoop([l_sb2, l_v2_m, -l_st2, -l_v1_m])])
+    s7 = geom.addPlaneSurface([geom.addCurveLoop([l_sb3, l_v3_m, -l_st3, -l_v2_m])])
 
-    cl5_arc = geom.addCurveLoop([l_cowl_arc, l_v4_top, -l_top_4_arc, -l_v4_bot])
-    s5_arc = geom.addPlaneSurface([cl5_arc])
-
-    cl6 = geom.addCurveLoop([l_cowl_back, l_out_ext, -l_top_5, -l_v4_top])
-    s6 = geom.addPlaneSurface([cl6])
+    s8 = geom.addPlaneSurface([geom.addCurveLoop([l_st1, l_v1_t, -l_t1, -l_v0_t])])
+    s9 = geom.addPlaneSurface([geom.addCurveLoop([l_st2, l_v2_t, -l_t2, -l_v1_t])])
+    s10 = geom.addPlaneSurface([geom.addCurveLoop([l_st3, l_v3_t, -l_t3, -l_v2_t])])
+    s11 = geom.addPlaneSurface([geom.addCurveLoop([l_ct1, l_v4, -l_t4, -l_v3_t])])
+    s12 = geom.addPlaneSurface([geom.addCurveLoop([l_ct2, l_v5_t, -l_t5, -l_v4])])
 
     geom.synchronize()
 
-    for line in [l_bottom, l_mid_1, l_top_1]:
+    for line in [l_b1, l_sb1, l_st1, l_t1]:
         mesh.setTransfiniteCurve(line, nx_1)
-    for line in [l_wall_1, l_mid_2, l_top_2]:
+    for line in [l_b2, l_sb2, l_st2, l_t2]:
         mesh.setTransfiniteCurve(line, nx_2)
-    for line in [l_wall_2, l_mid_3, l_top_3]:
+    for line in [l_b3, l_sb3, l_st3, l_t3]:
         mesh.setTransfiniteCurve(line, nx_3)
-    for line in [l_throat_bot, l_throat_top]:
+    for line in [l_b4, l_sb4]:
         mesh.setTransfiniteCurve(line, nx_throat)
-
-    for line in [l_cowl_front, l_top_4_front]:
-        mesh.setTransfiniteCurve(line, nx_cowl_front)
-    for line in [l_cowl_arc, l_top_4_arc]:
-        mesh.setTransfiniteCurve(line, nx_cowl_arc)
-    for line in [l_cowl_back, l_top_5]:
+    for line in [l_ct1, l_t4]:
+        mesh.setTransfiniteCurve(line, nx_cowl)
+    for line in [l_ct2, l_t5]:
         mesh.setTransfiniteCurve(line, nx_wake)
 
-    for line in [l_inlet_bot, l_v1_bot, l_v2_bot]:
-        mesh.setTransfiniteCurve(line, ny_bottom, "progression".capitalize(), 1.005)
-    for line in [l_throat_inlet, l_out_int]:
-        mesh.setTransfiniteCurve(line, ny_bottom, "bump".capitalize(), 0.05)
-    for line in [l_inlet_top, l_v1_top, l_v2_top, l_v3, l_v4_bot, l_v4_top, l_out_ext]:
-        mesh.setTransfiniteCurve(line, ny_top, "progression".capitalize(), 1.01)
+    for line in [l_v0_b, l_v1_b, l_v2_b, l_v3_b, l_v5_b]:
+        mesh.setTransfiniteCurve(line, ny_bottom, "Progression", 1.005)
 
-    mesh.setTransfiniteSurface(s1_b, cornerTags=[p1, p2, p7, p6])
-    mesh.setTransfiniteSurface(s2_b, cornerTags=[p2, p3, p8, p7])
-    mesh.setTransfiniteSurface(s3_b, cornerTags=[p3, p4, p9, p8])
-    mesh.setTransfiniteSurface(s1_t, cornerTags=[p6, p7, p14, p13])
-    mesh.setTransfiniteSurface(s2_t, cornerTags=[p7, p8, p15, p14])
-    mesh.setTransfiniteSurface(s3_t, cornerTags=[p8, p9, p16, p15])
-    mesh.setTransfiniteSurface(s4, cornerTags=[p4, p5, p12, p9])
-    mesh.setTransfiniteSurface(s5_front, cornerTags=[p9, p10_bot, p17_bot, p16])
-    mesh.setTransfiniteSurface(s5_arc, cornerTags=[p10_bot, p10_top, p17_top, p17_bot])
-    mesh.setTransfiniteSurface(s6, cornerTags=[p10_top, p11, p18, p17_top])
+    for line in [l_v0_m, l_v1_m, l_v2_m, l_v3_m]:
+        mesh.setTransfiniteCurve(line, ny_mid)
 
-    surfaces = [
-        (2, s1_b),
-        (2, s2_b),
-        (2, s3_b),
-        (2, s1_t),
-        (2, s2_t),
-        (2, s3_t),
-        (2, s4),
-        (2, s5_front),
-        (2, s5_arc),
-        (2, s6),
-    ]
+    for line in [l_v0_t, l_v1_t, l_v2_t, l_v3_t, l_v4, l_v5_t]:
+        mesh.setTransfiniteCurve(line, ny_top, "Progression", 1.01)
+
+    mesh.setTransfiniteSurface(s1, cornerTags=[p1, p2, p7, p6])
+    mesh.setTransfiniteSurface(s2, cornerTags=[p2, p3, p8, p7])
+    mesh.setTransfiniteSurface(s3, cornerTags=[p3, p4, p9_bot, p8])
+    mesh.setTransfiniteSurface(s4, cornerTags=[p4, p5, p12, p9_bot])
+
+    mesh.setTransfiniteSurface(s5, cornerTags=[p6, p7, p_mt2, p_mt1])
+    mesh.setTransfiniteSurface(s6, cornerTags=[p7, p8, p_mt3, p_mt2])
+    mesh.setTransfiniteSurface(s7, cornerTags=[p8, p9_bot, p9_top, p_mt3])
+
+    mesh.setTransfiniteSurface(s8, cornerTags=[p_mt1, p_mt2, p14, p13])
+    mesh.setTransfiniteSurface(s9, cornerTags=[p_mt2, p_mt3, p15, p14])
+    mesh.setTransfiniteSurface(s10, cornerTags=[p_mt3, p9_top, p16, p15])
+    mesh.setTransfiniteSurface(s11, cornerTags=[p9_top, p10, p17, p16])
+    mesh.setTransfiniteSurface(s12, cornerTags=[p10, p11, p18, p17])
+
+    all_surfaces = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12]
+    for s in all_surfaces:
+        mesh.setRecombine(2, s)
+
+    surfaces_extrude = [(2, s) for s in all_surfaces]
     extruded_entities = geom.extrude(
-        surfaces, 0, 0, z_extrude, numElements=[nz_total], recombine=True
+        surfaces_extrude, 0, 0, z_extrude, numElements=[nz_total], recombine=True
     )
 
     geom.synchronize()
@@ -219,7 +201,7 @@ def main():
     gmsh.option.setNumber("Mesh.RecombineAll", 1)
     gmsh.option.setNumber("Mesh.Smoothing", 10)
     mesh.generate(3)
-    gmsh.write("mesh_3d.msh")
+    gmsh.fltk.run()
     gmsh.finalize()
 
 
